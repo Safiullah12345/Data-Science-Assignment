9f8d0fcde570df3d2e01bfe4377c1f794f40eeda
Safiullah12345 <96950884+Safiullah12345@users.noreply.github.com>
2022-01-01 23:38:52 +0500

    Add files via upload

---
+++ Assignment#1.py
@@ -0,0 +1,21 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+print("twinkle twinkle little star");
+print("      How I wonder what you are!");
+print("            Up above the world so high,");
+print("            like a diamond in the sky.");
+print("twinkle twinkle little star");
+print("      How I wonder what you are!");
+import platform
+print(platform.python_version())
+from datetime import datetime
+print("Today's date time:", datetime.now());
+
+
